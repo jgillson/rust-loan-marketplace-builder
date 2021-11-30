f96a8a5092708c19efb88d6d79e79da37452474c
John Gillson <jgillson@gmail.com>
2021-11-29 20:19:38 -0800

    ✨ Replace the Iron Framework with the Actix-web Framework

--- db/init.sql
+++ db/init.sql
@@ -9,7 +9,7 @@ CREATE TABLE events
     PRIMARY KEY (aggregate_type, aggregate_id, sequence)
 );
 
-CREATE TABLE group_query
+CREATE TABLE lender_group_query
 (
     query_instance_id text                        NOT NULL,
     version           bigint CHECK (version >= 0) NOT NULL,
@@ -17,5 +17,16 @@ CREATE TABLE group_query
     PRIMARY KEY (query_instance_id)
 );
 
+-- this table is only needed if snapshotting is employed
+CREATE TABLE snapshots
+(
+    aggregate_type text                              NOT NULL,
+    aggregate_id   text                              NOT NULL,
+    last_sequence  bigint CHECK (last_sequence >= 0) NOT NULL,
+    payload        text                              NOT NULL,
+    timestamp      timestamp with time zone DEFAULT (CURRENT_TIMESTAMP),
+    PRIMARY KEY (aggregate_type, aggregate_id, last_sequence)
+);
+
 CREATE USER test_user WITH ENCRYPTED PASSWORD 'test_pass';
-GRANT ALL PRIVILEGES ON DATABASE postgres TO test_user;
+GRANT ALL PRIVILEGES ON DATABASE lender_groups TO test_user;
--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,12 @@
 #![deny(clippy::all)]
 
 use std::collections::HashMap;
-use std::io::Read;
+use std::str;
 
+use futures::StreamExt;
+use actix_web::{App, Error, HttpRequest, HttpResponse, HttpServer, Responder, Result, web};
 use cqrs_es::AggregateError;
-use iron::{Headers, Iron, IronResult, Request, Response, status};
 use postgres_es::{Connection, GenericQueryRepository, PostgresCqrs};
-use router::Router;
 
 use crate::aggregate::LenderGroup;
 use crate::queries::{LenderGroupQuery, SimpleLoggingQueryProcessor};
@@ -18,36 +18,42 @@ mod commands;
 mod events;
 mod queries;
 
-fn main() {
-    let mut router = Router::new();
-    router.get("/lender_group/:query_id", lender_group_query, "lender_group_query");
-    router.post("/lender_group/:command_type/:aggregate_id", lender_group_command, "lender_group_command");
-    println!("Starting server at http://localhost:3031");
-    Iron::new(router).http("localhost:3031").unwrap();
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .route("/lender_group/{query_id}", web::get().to(lender_group_query))
+            .route("/lender_group/{command_type}/{aggregate_id}", web::post().to(lender_group_command))
+    })
+        .bind("127.0.0.1:3031")?
+        .run()
+        .await
 }
 
-pub fn lender_group_command(req: &mut Request) -> IronResult<Response> {
-    let params = req.extensions.get::<Router>().unwrap();
-    let command_type = params.find("command_type").unwrap_or("");
-    let aggregate_id = params.find("aggregate_id").unwrap_or("");
-    let mut payload = String::new();
-    req.body.read_to_string(&mut payload).unwrap();
+async fn lender_group_command(req: HttpRequest, mut body: web::Payload) -> Result<HttpResponse, Error> {
+    let command_type = req.match_info().get("command_type").unwrap_or("");
+    let aggregate_id = req.match_info().get("aggregate_id").unwrap_or("");
+    let mut bytes = web::BytesMut::new();
+    while let Some(item) = body.next().await {
+        bytes.extend_from_slice(&item?);
+    }
+
+    let byte_str = str::from_utf8(&bytes).unwrap();
+    let payload = byte_str.replace(&['\n', '\t'][..], "");
     let result = match command_type {
         "addLenderGroup" => process_command("AddLenderGroup", aggregate_id, payload),
         "addLender" => process_command("AddLender", aggregate_id, payload),
         "removeLender" => process_command("RemoveLender", aggregate_id, payload),
-        _ => return Ok(Response::with(status::NotFound))
+        _ => Ok(())
     };
     match result {
-        Ok(_) => Ok(Response::with(status::NoContent)),
+        Ok(_) => Ok(HttpResponse::Ok().body("no content")),
         Err(err) => {
             let err_payload = match &err {
                 AggregateError::UserError(e) => serde_json::to_string(e).unwrap(),
-                AggregateError::TechnicalError(e) => e.clone(),
+                AggregateError::TechnicalError(e)=> e.clone(),
             };
-            let mut response = Response::with((status::BadRequest, err_payload));
-            response.headers = std_headers();
-            Ok(response)
+            Err(Error::from(HttpResponse::Ok().body(err_payload)))
         }
     }
 }
@@ -66,38 +72,30 @@ fn process_command(payload_type: &str, aggregate_id: &str, payload: String) -> R
     cqrs.execute_with_metadata(aggregate_id, payload, metadata)
 }
 
-pub fn lender_group_query(req: &mut Request) -> IronResult<Response> {
-    let query_id = req.extensions.get::<Router>().unwrap().find("query_id").unwrap_or("").to_string();
+async fn lender_group_query(req: HttpRequest) -> impl Responder {
+    let query_id = req.match_info().get("query_id").unwrap_or("").to_string();
+    let query_repo = LenderGroupQueryRepository::new("lender_group_query", db_connection());
 
-    let query_repo = GroupQuery::new("group_query", db_connection());
     match query_repo.load(query_id) {
         None => {
-            Ok(Response::with(status::NotFound))
+            HttpResponse::Ok().body("none")
         }
         Some(query) => {
             let body = serde_json::to_string(&query).unwrap();
-            let mut response = Response::with((status::Ok, body));
-            response.headers = std_headers();
-            Ok(response)
+            HttpResponse::Ok().content_type("application/json").body(body)
         }
     }
 }
 
-fn std_headers() -> Headers {
-    let mut headers = Headers::new();
-    let content_type = iron::headers::ContentType::json();
-    headers.set(content_type);
-    headers
-}
-
-type GroupQuery = GenericQueryRepository::<LenderGroupQuery, LenderGroup>;
+type LenderGroupQueryRepository = GenericQueryRepository::<LenderGroupQuery, LenderGroup>;
 
 fn cqrs_framework() -> PostgresCqrs<LenderGroup> {
     let simple_query = SimpleLoggingQueryProcessor {};
-    let mut group_query_processor = GroupQuery::new("group_query", db_connection());
-    group_query_processor.with_error_handler(Box::new(|e| println!("{}", e)));
+    let mut lender_group_query_processor =
+        LenderGroupQueryRepository::new("lender_group_query", db_connection());
+    lender_group_query_processor.with_error_handler(Box::new(|e| println!("{}", e)));
 
-    postgres_es::postgres_cqrs(db_connection(), vec![Box::new(simple_query), Box::new(group_query_processor)])
+    postgres_es::postgres_cqrs(db_connection(), vec![Box::new(simple_query), Box::new(lender_group_query_processor)])
 }
 
 fn db_connection() -> Connection {
